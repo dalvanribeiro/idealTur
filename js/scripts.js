42388e25e40fb88765c99a7b6fa73f6f105877da
Dalvan Ribeiro de Almeida <dalvanra@hotmail.com>
2021-10-08 15:46:40 -0300

    pequenos ajustes

--- js/scripts.js
+++ js/scripts.js
@@ -75,10 +75,10 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
-    // Activate SimpleLightbox plugin for portfolio items
-    new SimpleLightbox({
-        elements: '#portfolio a.portfolio-box'
-    });
+    // // Activate SimpleLightbox plugin for portfolio items
+    // new SimpleLightbox({
+    //     elements: '#portfolio a.portfolio-box'
+    // });
 
 });
 
